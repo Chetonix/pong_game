c4723af16c247a6e05793a2e88e48ed089802991
chetanharjani <chetonix@gmail.com>
2024-08-05 20:38:24 +0530

    add ball logic to the game, next is collision

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from turtle import Turtle, Screen
 from paddle import Paddle
+from ball import Ball
+import time
 
 wn = Screen()
 wn.setup(800, 600)
@@ -10,6 +12,7 @@ wn.tracer(0)
 l_paddle = Paddle((-350, 0))
 r_paddle = Paddle((350, 0))
 
+ball = Ball()
 
 
 wn.listen()
@@ -21,6 +24,8 @@ wn.onkey(l_paddle.go_down, "s")
 
 game_is_on = True
 while game_is_on:
+    time.sleep(0.1)
     wn.update()
+    ball.move()
 
 wn.exitonclick()
