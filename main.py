4bc58da92137da54f64f5f49ea2e147891a5c649
chetanharjani <chetonix@gmail.com>
2024-08-05 20:06:24 +0530

    create a Paddle class to handle paddle functionality

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
 from turtle import Turtle, Screen
+from paddle import Paddle
 
 wn = Screen()
 wn.setup(800, 600)
 wn.bgcolor("black")
 wn.title("Pong")
+wn.tracer(0)
+
+l_paddle = Paddle((-350, 0))
+r_paddle = Paddle((350, 0))
 
 
 
+wn.listen()
+wn.onkey(r_paddle.go_up, "Up")
+wn.onkey(r_paddle.go_down, "Down")
 
-
+game_is_on = True
+while game_is_on:
+    wn.update()
 
 wn.exitonclick()
