f7628989cacad6c6fd80eda820ac72ca79c59198
chetanharjani <chetonix@gmail.com>
2024-08-06 14:20:48 +0530

    add code for ball going out of bounds

--- main.py
+++ main.py
@@ -37,4 +37,12 @@ while game_is_on:
     if (ball.distance(r_paddle) < 50 and ball.xcor() > 320) or (ball.distance(l_paddle) < 50 and ball.xcor() < - 320):
         ball.bounce_x()
 
+    #detect if r_paddle misses the ball
+    if ball.xcor() > 380:
+        ball.reset_position()
+
+    #detect if l_paddle misses the ball
+    if ball.xcor() < -380:
+        ball.reset_position()
+
 wn.exitonclick()
